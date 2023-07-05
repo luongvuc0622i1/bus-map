b05842068a5a40bc1d1e705ef9cd86e5a9689eb3
luongvuc0622i1 <vuhienluong.td151218@gmail.com>
2023-07-05 18:02:33 +0700

    8

---
+++ src/json/bus-stop/bn86a.js
@@ -0,0 +1,398 @@
+export let bus_stop_list_bn86a = {
+    'type': 'FeatureCollection',
+    'features': [
+    {
+      'type': 'Feature',
+      'geometry': {
+        'type': 'Point',
+        'coordinates': [106.061027,21.180761]
+      },
+      'properties': {
+        'title': '(A) Bến xe Bắc Ninh',
+        'description': '2 Nguyễn Du, Thành phố Bắc Ninh',
+        'router': 'BN01, BN02, BN03, BN08, BN86A, BN86B, 54, 203'
+      }
+    },
+    {
+      'type': 'Feature',
+      'geometry': {
+        'type': 'Point',
+        'coordinates': [106.059702,21.178827]
+      },
+      'properties': {
+        'title': 'Hiệu sách nhân dân tỉnh Bắc Ninh',
+        'description': '687 Ngô Gia Tự, Thành phố Bắc Ninh',
+        'router': 'BN01, BN02, BN03, BN08, BN86A, BN86B, 54, 203'
+      },
+      'color': 'blue'
+    },
+    {
+      'type': 'Feature',
+        'geometry': {
+          'type': 'Point',
+          'coordinates': [106.064174,21.180864]
+      },
+      'properties': {
+        'title': 'Chợ Nhớn',
+        'description': '60 Trần Hưng Đạo, Thành phố Bắc Ninh',
+        'router': 'BN02, BN68A, BN68B'
+      },
+      'color': 'blue'
+    },
+    {
+      'type': 'Feature',
+        'geometry': {
+          'type': 'Point',
+          'coordinates': [106.065580,21.180400]
+      },
+      'properties': {
+        'title': 'Trạm Y tế Phường Tiền An',
+        'description': '117 Trần Hưng Đạo, Thành phố Bắc Ninh',
+        'router': 'BN02, BN68A, BN68B'
+      },
+      'color': 'red'
+    },
+    {
+      'type': 'Feature',
+        'geometry': {
+          'type': 'Point',
+          'coordinates': [106.069870,21.178600]
+      },
+      'properties': {
+        'title': 'HimLam Plaza',
+        'description': '',
+        'router': 'BN02, BN68A, BN68B'
+      }
+    },
+    {
+      'type': 'Feature',
+        'geometry': {
+          'type': 'Point',
+          'coordinates': [106.072246,21.177559]
+      },
+      'properties': {
+        'title': 'Tòa Vietcombank Bắc Ninh',
+        'description': '',
+        'router': 'BN02, BN68A, BN68B, 212'
+      },
+      'color': 'blue'
+    },
+    {
+      'type': 'Feature',
+        'geometry': {
+          'type': 'Point',
+          'coordinates': [106.072377,21.177637]
+      },
+      'properties': {
+        'title': 'Vincom Plaza Lý Thái Tổ',
+        'description': '',
+        'router': 'BN02, BN68A, BN68B, 212'
+      },
+      'color': 'red'
+    },
+    {
+      'type': 'Feature',
+        'geometry': {
+          'type': 'Point',
+          'coordinates': [106.079279,21.174902]
+      },
+      'properties': {
+        'title': 'Khu tập thể Lilama 69-1',
+        'description': '112 Trần Hưng Đạo, Thành phố Bắc Ninh',
+        'router': 'BN02, BN68A, BN68B, 212'
+      },
+      'color': 'blue'
+    },
+    {
+      'type': 'Feature',
+        'geometry': {
+          'type': 'Point',
+          'coordinates': [106.079332,21.175007]
+      },
+      'properties': {
+        'title': '',
+        'description': '131 Trần Hưng Đạo, Thành phố Bắc Ninh',
+        'router': 'BN02, BN68A, BN68B, 212'
+      },
+      'color': 'red'
+    },
+    {
+      'type': 'Feature',
+        'geometry': {
+          'type': 'Point',
+          'coordinates': [106.083540,21.173804]
+      },
+      'properties': {
+        'title': 'Nhà khách Bộ chỉ huy Quân sự Bắc Ninh',
+        'description': '232 Trần Hưng Đạo, Thành phố Bắc Ninh',
+        'router': 'BN02, BN68A, BN68B, 212'
+      },
+      'color': 'blue'
+    },
+    {
+      'type': 'Feature',
+        'geometry': {
+          'type': 'Point',
+          'coordinates': [106.084349,21.173756]
+      },
+      'properties': {
+        'title': 'Cổng chào Thành phố Bắc Ninh',
+        'description': '',
+        'router': 'BN02, BN68A, BN68B, 212'
+      },
+      'color': 'red'
+    },
+    {
+      'type': 'Feature',
+        'geometry': {
+          'type': 'Point',
+          'coordinates': [106.094880,21.171406]
+      },
+      'properties': {
+        'title': 'Cây xăng Cầu Ngà',
+        'description': '',
+        'router': 'BN02, BN68A, BN68B, 212'
+      }
+    },
+    {
+      'type': 'Feature',
+        'geometry': {
+          'type': 'Point',
+          'coordinates': [106.101941,21.169465]
+      },
+      'properties': {
+        'title': 'Siêu thị Minh Anh',
+        'description': '',
+        'router': 'BN02, BN68A, BN68B, 212'
+      }
+    },
+    {
+      'type': 'Feature',
+        'geometry': {
+          'type': 'Point',
+          'coordinates': [106.110034,21.163219]
+      },
+      'properties': {
+        'title': 'Công ty TNHH Canon Vietnam - KCN Quế Võ',
+        'description': '',
+        'router': 'BN02, BN68A, BN68B, 212'
+      }
+    },
+    {
+      'type': 'Feature',
+        'geometry': {
+          'type': 'Point',
+          'coordinates': [106.116185,21.159056]
+      },
+      'properties': {
+        'title': 'Công ty Sài Gòn Kinh Bắc',
+        'description': '',
+        'router': 'BN02, BN68A, BN68B, 212'
+      }
+    },
+    {
+      'type': 'Feature',
+        'geometry': {
+          'type': 'Point',
+          'coordinates': [106.126529,21.157415]
+      },
+      'properties': {
+        'title': 'Cổng Làng Giang Liễu',
+        'description': '',
+        'router': 'BN02, BN68A, BN68B, 212'
+      }
+    },
+    {
+      'type': 'Feature',
+        'geometry': {
+          'type': 'Point',
+          'coordinates': [106.136341,21.155789]
+      },
+      'properties': {
+        'title': 'Trường Cao Đẳng nghề Quản Lý và Công Nghệ',
+        'description': '',
+        'router': 'BN02, BN68A, BN68B, 212'
+      }
+    },
+    {
+      'type': 'Feature',
+        'geometry': {
+          'type': 'Point',
+          'coordinates': [106.145730,21.154256]
+      },
+      'properties': {
+        'title': 'Siêu thị DABACO Quế Võ',
+        'description': '',
+        'router': 'BN02, BN68A, BN68B, 212'
+      }
+    },
+    {
+      'type': 'Feature',
+        'geometry': {
+          'type': 'Point',
+          'coordinates': [106.150950,21.153405]
+      },
+      'properties': {
+        'title': 'Cửa hàng vàng bạc Xuân Mạo',
+        'description': '183 Trần Hưng Đạo, Quế Võ',
+        'router': 'BN02, BN68A, BN68B, 212'
+      }
+    },
+    {
+      'type': 'Feature',
+        'geometry': {
+          'type': 'Point',
+          'coordinates': [106.154165,21.152895]
+      },
+      'properties': {
+        'title': 'Công an Thị xã Quế Võ',
+        'description': 'Khu 1, Phố Mới, Quế Võ',
+        'router': 'BN02, BN68A, BN68B, 212'
+      }
+    },
+    {
+      'type': 'Feature',
+        'geometry': {
+          'type': 'Point',
+          'coordinates': [106.155843,21.152598]
+      },
+      'properties': {
+        'title': 'Bến xe Quế Võ',
+        'description': '',
+        'router': 'BN02, BN68A, BN68B, 212'
+      }
+    },
+    {
+      'type': 'Feature',
+        'geometry': {
+          'type': 'Point',
+          'coordinates': [106.167044,21.148198]
+      },
+      'properties': {
+        'title': 'UBND Phường Việt Hùng',
+        'description': '',
+        'router': 'BN02, BN68A, BN68B'
+      }
+    },
+    {
+      'type': 'Feature',
+        'geometry': {
+          'type': 'Point',
+          'coordinates': [106.174731,21.140997]
+      },
+      'properties': {
+        'title': 'Phố Lựa',
+        'description': 'Gần Chùa Khánh Quang',
+        'router': 'BN02, BN68A, BN68B'
+      }
+    },
+    {
+      'type': 'Feature',
+        'geometry': {
+          'type': 'Point',
+          'coordinates': [106.179127,21.137661]
+      },
+      'properties': {
+        'title': 'KCN Quế Võ III',
+        'description': 'Gần Chùa Diên Quang',
+        'router': 'BN02, BN68A, BN68B'
+      }
+    },
+    {
+      'type': 'Feature',
+        'geometry': {
+          'type': 'Point',
+          'coordinates': [106.188670,21.132535]
+      },
+      'properties': {
+        'title': 'Trung tâm Thánh mẫu Từ Phong',
+        'description': '',
+        'router': 'BN02, BN68A, BN68B'
+      }
+    },
+    {
+      'type': 'Feature',
+        'geometry': {
+          'type': 'Point',
+          'coordinates': [106.198230,21.128221]
+      },
+      'properties': {
+        'title': 'Agribank Đông Du',
+        'description': '',
+        'router': 'BN02, BN68A, BN68B'
+      }
+    },
+    {
+      'type': 'Feature',
+        'geometry': {
+          'type': 'Point',
+          'coordinates': [106.223544,21.123041]
+      },
+      'properties': {
+        'title': 'KCN Quế Võ II',
+        'description': '',
+        'router': 'BN02, BN68A, BN68B'
+      }
+    },
+    {
+      'type': 'Feature',
+        'geometry': {
+          'type': 'Point',
+          'coordinates': [106.237439,21.121243]
+      },
+      'properties': {
+        'title': 'Thôn Ngọc Sơn',
+        'description': '',
+        'router': 'BN02, BN68A, BN68B'
+      }
+    },
+    {
+      'type': 'Feature',
+        'geometry': {
+          'type': 'Point',
+          'coordinates': [106.243259,21.120507]
+      },
+      'properties': {
+        'title': 'Chân cầu Bình Than',
+        'description': '',
+        'router': 'BN02, BN68A, BN68B'
+      }
+    },
+    {
+      'type': 'Feature',
+        'geometry': {
+          'type': 'Point',
+          'coordinates': [106.249467,21.105088]
+      },
+      'properties': {
+        'title': 'Thôn Chính Thượng',
+        'description': '',
+        'router': 'BN68A, BN68B'
+      }
+    },
+    {
+      'type': 'Feature',
+        'geometry': {
+          'type': 'Point',
+          'coordinates': [106.251166,21.098230]
+      },
+      'properties': {
+        'title': 'KCN Hanaka Gia Bình II',
+        'description': '',
+        'router': 'BN68A, BN68B'
+      }
+    },
+    {
+      'type': 'Feature',
+        'geometry': {
+          'type': 'Point',
+          'coordinates': [106.265589,20.989173]
+      },
+      'properties': {
+        'title': '(B) Minh Tân',
+        'description': 'Gần chợ Văn Thai',
+        'router': 'BN68A'
+      }
+    }
+    ]
+  }
